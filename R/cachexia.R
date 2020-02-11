99086cd01de45efc163268dbb882af5517f694af
Manuele Leonelli <manuele.leonelli@gmail.com>
2020-02-11 13:54:51 +0100

    Datasets Update

---
+++ R/cachexia.R
@@ -0,0 +1,35 @@
+#' Bayesian networks for a cachexia study
+#'
+#' Some comments here
+#'
+#' @docType data
+#'
+#' @family datasets
+#'
+#' @format The Bayesian network \code{bnsens_example} comprehends the following nodes:
+#' \itemize{
+#'   \item \bold{y1}: three-level factor with levels \emph{1}, \emph{2}, \emph{3}.
+#'   \item \bold{y2}: three-level factor with levels \emph{1}, \emph{2}, \emph{3}.
+#'   \item \bold{y3}:  three-level factor with levels \emph{1}, \emph{2}, \emph{3}.
+#' }
+#'
+#' @aliases cachexia_gbn
+#' @name cachexia
+#'
+#'@source	 Manuele Leonelli, Eva Riccomagno (2018). "A geometric characterisation of sensitivity analysis in monomial models". \url{https://arxiv.org/abs/1901.02058}
+NULL
+
+#'
+#' @rdname cachexia
+"cachexia_gbn"
+
+#' @rdname cachexia
+"cachexia_ci"
+
+#'
+#' @rdname cachexia
+"control_gbn"
+
+#' @rdname cachexia
+"control_ci"
+
